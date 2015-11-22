ba2a077e97ef3cfc56a7a8ffa78a60357a5d9dd1
Axure <axurez@vip.qq.com>
2015-11-22 12:21:44 +0800

    Separate the source and test.

---
+++ src/cpp/Bonus/Edge of BST/test/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#include "../lib/Bst.h"
+
+void print(int a) {
+  std::cout << a << ", ";
+}
+
+int main(int argc, char *argv[]) {
+  int v = 3;
+  auto *a = new BinaryTree<int>(v);
+  auto *b = new BinaryTree<int>(4);
+  auto *c = new BinaryTree<int>(5);
+  auto *d = new BinaryTree<int>(6);
+  a->setLeft(b);
+  b->setLeft(d);
+  a->setRight(c);
+
+
+  std::function<void(int)> f = print;
+
+  a->preOrderTraverse(f);
+  return 0;
+
+};

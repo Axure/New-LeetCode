b96b71d4f8c47e22f5f29e30d8526413c28a0d06
Axure <axurez@vip.qq.com>
2016-01-19 04:24:28 +0800

    Start to change the project layout.

---
+++ src/cpp/Algorithms/321 Create Maximum Number/GreatNumber.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 郑虎 on 2015-12-25.
+//
+
+#include "GreatNumber.h"
---
+++ src/cpp/Algorithms/321 Create Maximum Number/GreatNumber.h
@@ -0,0 +1,17 @@
+//
+// Created by 郑虎 on 2015-12-25.
+//
+
+#ifndef LEETCODE_GREATNUMBER_H
+#define LEETCODE_GREATNUMBER_H
+
+#include<vector>
+
+class GreatNumber {
+ private:
+  vector<char>
+ public:
+
+};
+
+#endif //LEETCODE_GREATNUMBER_H
